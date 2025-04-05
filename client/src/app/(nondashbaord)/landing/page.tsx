a72416c4008a0500b0c49b064007e1b94c528b22
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-04-06 07:46:49 +0800

    Add landing pages components

--- client/src/app/(nondashbaord)/landing/page.tsx
+++ client/src/app/(nondashbaord)/landing/page.tsx
@@ -1,9 +1,17 @@
+import CallToActionSection from '@/app/(nondashbaord)/landing/CallToActionSection';
+import DiscoverSection from '@/app/(nondashbaord)/landing/DiscoverSection';
+import FeaturesSection from '@/app/(nondashbaord)/landing/FeaturesSection';
+import FooterSection from '@/app/(nondashbaord)/landing/FooterSection';
 import HeroSection from '@/app/(nondashbaord)/landing/HeroSection';
 
 const LandingPage = () => {
   return (
     <div>
       <HeroSection />
+      <FeaturesSection />
+      <DiscoverSection />
+      <CallToActionSection />
+      <FooterSection />
     </div>
   );
 };
--- client/src/app/(nondashbaord)/layout.tsx
+++ client/src/app/(nondashbaord)/layout.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 
 import Navbar from '@/components/Navbar';
-import { NAVBAR_HEIGHT } from '@/lib/constants';
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full w-full">
       <Navbar />
-      <main
-        className={`h-full flex w-full flex-col`}
-        style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}
-      >
-        {children}
-      </main>
+      <main className="h-full flex w-full flex-col">{children}</main>
     </div>
   );
 };
