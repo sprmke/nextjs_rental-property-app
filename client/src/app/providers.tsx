12245861e9e5731b1cc3894c9380025ca7f98b0d
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-04-08 05:19:51 +0800

    Add AuthProvider from aws-amplify/ui-react & our custom auth provider

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -1,9 +1,18 @@
 'use client';
 
+import { Authenticator } from '@aws-amplify/ui-react';
+
 import StoreProvider from '@/state/redux';
+import Auth from '@/app/(auth)/authProvider';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <StoreProvider>{children}</StoreProvider>;
+  return (
+    <StoreProvider>
+      <Authenticator.Provider>
+        <Auth>{children}</Auth>
+      </Authenticator.Provider>
+    </StoreProvider>
+  );
 };
 
 export default Providers;
