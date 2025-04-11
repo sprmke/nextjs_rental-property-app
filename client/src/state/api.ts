b394bae9ada691dab612378b6f0c4381916f326f
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-04-11 23:45:55 +0800

    Add update manager settings API endpoint on client

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -75,7 +75,28 @@ export const api = createApi({
         });
       },
     }),
+    updateManagerSettings: build.mutation<
+      Manager,
+      { cognitoId: string } & Partial<Manager>
+    >({
+      query: ({ cognitoId, ...updatedManager }) => ({
+        url: `managers/${cognitoId}`,
+        method: 'PUT',
+        body: updatedManager,
+      }),
+      invalidatesTags: (result) => [{ type: 'Managers', id: result?.id }],
+      async onQueryStarted(_, { queryFulfilled }) {
+        await withToast(queryFulfilled, {
+          success: 'Settings updated successfully!',
+          error: 'Failed to update settings.',
+        });
+      },
+    }),
   }),
 });
 
-export const { useGetAuthUserQuery, useUpdateTenantSettingsMutation } = api;
+export const {
+  useGetAuthUserQuery,
+  useUpdateTenantSettingsMutation,
+  useUpdateManagerSettingsMutation,
+} = api;
