c979271490247f40e6974f5f27f96c647818c257
Michael Manlulu <michaeldmanlulu@gmail.com>
2025-04-11 23:35:25 +0800

    Add route guard to nondashboard paths

--- client/src/app/(nondashbaord)/layout.tsx
+++ client/src/app/(nondashbaord)/layout.tsx
@@ -1,11 +1,44 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
+import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+
+import { Loader2 } from 'lucide-react';
+
 import Navbar from '@/components/Navbar';
 import { useGetAuthUserQuery } from '@/state/api';
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  const { data: authUser } = useGetAuthUserQuery();
-  console.log('authUser::', authUser);
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const { data: authUser, isLoading: authLoading } = useGetAuthUserQuery();
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (authUser) {
+      const userRole = authUser.userRole?.toLowerCase();
+      if (
+        (userRole === 'manager' && pathname.startsWith('/search')) ||
+        (userRole === 'manager' && pathname === '/')
+      ) {
+        router.push('/managers/properties', { scroll: false });
+      } else {
+        setIsLoading(false);
+      }
+    }
+  }, [authUser, router, pathname]);
+
+  if (authLoading || isLoading)
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Loader2 className="w-10 h-10 animate-spin opacity-50" />
+      </div>
+    );
+
   return (
     <div className="h-full w-full">
       <Navbar />
